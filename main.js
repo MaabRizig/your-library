bd027922b0cc7cb825beee8eb932775a528283c9
MaabRizig <116944226+MaabRizig@users.noreply.github.com>
2026-09-05 14:12:17 +0300

    create delete book functionality & link it to delete menu option

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 let booksArr = [];
+const BOOKSCONTAINER = document.querySelector(".books-container");
 
-function Book(author,title,pageCount,readStatus){
+function Book(title,author,pageCount,readStatus){
     this.id = crypto.randomUUID();
-    this.author = author;
     this.title = title;
+    this.author = author;
     this.pageCount = pageCount;
     this.readStatus = readStatus;
 }
@@ -13,18 +14,19 @@ Book.prototype.readStatusToText = function(){
     return VALUETOTEXT[this.readStatus];
 }
 
-function addBook(author,title,pageCount,readStatus){
-    let bookObj = new Book(author,title,pageCount,readStatus);
+
+function addBook(title,author,pageCount,readStatus){
+    let bookObj = new Book(title,author,pageCount,readStatus);
     booksArr.push(bookObj);
     return bookObj;
 }
 
 function displayBooks(){
-    const BOOKSCONTAINER = document.querySelector(".books-container");
-
+    BOOKSCONTAINER.textContent = "";
     booksArr.forEach((book)=>{
         let bookDiv = document.createElement("div");
         bookDiv.classList.add("book");
+        bookDiv.dataset.id = book.id;
         let menuIcon = document.createElement("span");
         menuIcon.classList.add("book-menu","icon");
         let infoList = document.createElement("ul");
@@ -46,57 +48,86 @@ function displayBooks(){
     })
 }
 
+function deleteBook(id){
+    let bookIndex = booksArr.findIndex((book)=>{
+        if(book.id === id)
+            return true;
+        return false;
+    });
+    if(typeof bookIndex === 'number'){
+        booksArr.splice(bookIndex,1);
+        displayBooks();
+    } else{
+        throw Error("no book with this id");
+    }
+}
+
 
 let addBookBtn = document.querySelector(".add-book");
 let addBookDialog = document.querySelector("#add-dialog");
 let submitBtn = document.querySelector("#submit-button");
 let form = document.querySelector("#add-dialog form");
+let menuDialog = document.querySelector(".menu");
 
 
-addBookBtn.addEventListener("click",()=>{
+let showFormDialogListener = addBookBtn.addEventListener("click",()=>{
     addBookDialog.showModal();
 });
 
 
-form.addEventListener("submit",(e)=>{
+let submitFormInfoListener = form.addEventListener("submit",(e)=>{
     e.preventDefault();
     const formData = new FormData(form);
     const dataObj = Object.fromEntries(formData.entries());
-    console.log(dataObj );
     if(dataObj.title !== ""){
         addBookDialog.close();
-        addBook(dataObj.author,dataObj.title,dataObj.pageCount,dataObj.readStatus);
+        addBook(dataObj.title,dataObj.author,dataObj.pageCount,dataObj.readStatus);
         displayBooks();
     }
 });
 
 
-//let menuIcons = document.querySelectorAll(".book-menu");
-//menuIcons.addEventListener("contextmenu",(e)=>{
-
-// });
-
-
-
-
-
-
-
-
-
-
-
-// ------------------------
+let showDialogListener = BOOKSCONTAINER.addEventListener("click",(e)=>{           
+    if(e.target.classList.contains("book-menu")){
+        menuDialog.show();
+        let bookParent = e.target.closest(".book"); 
+        console.log(bookParent);
+        menuDialog.dataset.bookId = bookParent.dataset.id;
+        let menuDialogWidth = parseInt(window.getComputedStyle(menuDialog).getPropertyValue("width"));
+        menuDialog.style.left =` ${e.clientX - menuDialogWidth}px`;
+        menuDialog.style.top = `${e.clientY}px`;
+    }
+});
 
 
 
-// addBook("maab","how to make",454,1);
-// addBook("ali","how to be a bee",343,2);
-// addBook("maab","how to make",454,3);
-// addBook("ali","how to be a bee",343,3);
-// addBook("maab","how to make",454,2);
-// addBook("ali","how to be a bee",343,1);
-// console.log(booksArr);
-// displayBooks();
+menuDialog.addEventListener("click",(e)=>{
+    let optionEle = e.target.closest("[data-type]");
+    if(!optionEle)
+        return;
+    let optionType = optionEle.dataset.type;
+    switch (optionType){
+        case "deleteOpt":
+            let bookId = menuDialog.dataset.bookId;
+            deleteBook(bookId);
+            break;
+    }
+    menuDialog.close();
+}); 
+
+
+
+
+
+
+
+addBook("AI Engineering","Chip Huyen",454,2);
+addBook("Feature Engineering","Max Kuhn & Kjell Johnson",343,2);
+addBook("Practical MLOps","Noah Gift & Alfredo Deza",454,1);
+displayBooks();
+
+
+
+
 
 
